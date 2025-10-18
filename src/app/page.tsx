da9c316bee3eea4a5a2819b0e2cf1b2f304bd4f7
Wang Jian <wangjian0504@gmail.com>
2025-10-18 16:24:52 +0800

    add shadcn support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
-      <div>Hello World</div>
+    <div className="flex min-h-screen min-w-screen items-center justify-center">
+      <Button variant="outline">Click me</Button>
     </div>
   );
 }
