6de669460ff386a1c617a818b260a3f97e4b6a20
Wang Jian <wangjian0504@gmail.com>
2025-10-18 17:20:23 +0800

    add prisma support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
 
-export default function Home() {
+const Page = async () => {
+  const users = await prisma.user.findMany();
   return (
-    <div className="flex min-h-screen min-w-screen items-center justify-center">
+    <div className="flex min-h-screen min-w-screen flex-col items-center justify-center">
       <Button variant="outline">Click me</Button>
+      <code>{JSON.stringify(users)}</code>
     </div>
   );
-}
+};
+export default Page;
